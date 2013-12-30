a834002e2cfcdc237c564bab59b866e14b89cd5e
Scott Haselton <shaselton@gmail.com>
2013-12-30 14:07:01 -0800

    fun palindrome challenge

---
+++ palindrome/palindrome.js
@@ -0,0 +1,24 @@
+String.prototype.largestPalindrome = function(){
+	var words = this.toString().split(' '),
+		isPalindrome = function( word ){
+			var breakPoint = Math.floor( word.length / 2 ),
+				strSize = word.length - 1;
+			for( var i = 0; i < breakPoint; i++ ){
+				if( word[i] !== word[strSize-i] ){
+					return false;
+				}
+			}
+			return true;
+		},
+		largestWordCount = 0,
+		largestWord = "";
+
+	for( word in words ){
+		if( words[word].length > largestWordCount && isPalindrome( words[word] ) ){
+			largestWord = words[word];
+			largestWordCount = words[word].length;
+		}
+	}
+
+	return largestWord;
+}
